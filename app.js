dcdccea1d252c1127efc1f0faf3aac91e26caf8f
Chaambane <mohamed.faidine@live.fr>
2021-05-10 15:12:08 +0200

    feat (app) add helmet and cors

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 // Dépendances
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
 
 // Routes CRUD
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/user');
 const postRoute = require('./routes/post');
 const CommentRoute = require('./routes/comment');
+
+// Sécurisation 
+app.use(helmet());
+
+// Cors config
+app.use(cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}));
 
 const app = express();
 app.use(bodyParser.json());
