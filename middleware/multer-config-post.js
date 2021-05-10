68850929682ad90e9bfae15f1d2824c0d7364d3e
Chaambane <mohamed.faidine@live.fr>
2021-05-10 16:25:35 +0200

    refact (middleware) change file name

--- middleware/multer-config-post.js
+++ middleware/multer-config-post.js
@@ -1,3 +1,4 @@
+// Imporations
 const multer = require('multer');
 
 // Bibliothèque d'éxtension
@@ -8,16 +9,16 @@ const MIME_TYPES = {
     'image/gif':'gif'
 };
 
-// Enregistrement
+// Enregistrement des images
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, "public/images");
     },
-    filename: (req, file, cb) => {
+    filename: (req, file, cb) => { // Nom pour les images
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
       cb(null, name + Date.now() + '.' + extension);
-    },
+    }
 });
 
-module.exports = multer({ storage: storage }).single('file');
+module.exports = multer({ storage: storage }).single('image');
