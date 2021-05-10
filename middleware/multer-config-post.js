a3a5debdfb63f80b372c35c9ecc4b33836a7d9e3
Chaambane <mohamed.faidine@live.fr>
2021-05-10 15:34:02 +0200

    feat (public/images) add multer config

---
+++ middleware/multer-config-post.js
@@ -0,0 +1,23 @@
+const multer = require('multer');
+
+// Bibliothèque d'éxtension
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png',
+    'image/gif':'gif'
+};
+
+// Enregistrement
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+    const name = file.originalname.split(' ').join('_');
+    const extension = MIME_TYPES[file.mimetype];
+      cb(null, name + Date.now() + '.' + extension);
+    },
+});
+
+module.exports = multer({ storage: storage }).single('file');
