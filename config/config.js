42937faa76070887ae7378946f9a52c96fb1d6d0
Chaambane <mohamed.faidine@live.fr>
2021-05-10 08:14:19 +0200

    refact (project) change bcrypt to bcryptjs

--- config/config.js
+++ config/config.js
@@ -3,21 +3,21 @@ require('dotenv').config()
 module.exports = {
   development: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: null,
     database: process.env.DB_DEVELOPMENT,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT
   },
   test: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: null,
     database: process.env.DB_TEST,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT
   },
   production: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: null,
     database: process.env.DB_PRODUCTION,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT
